3c85696466df7bca620d99d68f45b8f3096d03cf
Petar <zigic@outlook.com>
2020-05-26 21:32:12 -0500

    Deleted or commented person references

--- src/main/java/com/pzigic/fund/manager/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pzigic.fund.manager.repository;
-
-import com.pzigic.fund.manager.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
